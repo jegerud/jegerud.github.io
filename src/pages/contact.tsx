0f5b3eff13d91799c06670ea9cd549031daac3ab
jegerud <jegerud98@gmail.com>
2026-05-31 17:56:46 +0200

    fix: adjust contact information

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,11 +13,6 @@ export function Contact() {
           <CustomLink href="https://www.linkedin.com/in/kristian-jegerud-bab1781a0/" target="_blank">
             LinkedIn Profile
           </CustomLink>
-          <Spacer>or</Spacer> 
-          feel free to stay in touch on 
-          <CustomLink href="https://www.threads.net/@jegerud98" target="_blank">
-            Threads
-          </CustomLink>
         </Content>
       </ContactHeader>
     </ContactContainer>
