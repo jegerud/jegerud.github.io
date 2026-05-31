0a7f959c0ff536aa5a256e07e92c44bdb4fdb715
jegerud <jegerud98@gmail.com>
2026-05-31 17:23:59 +0200

    feat: adjust navbar and app to include work experience

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export const NavBar = (props: Props) => {
   const setScrollPosition = (ref: any) => {
     const element = document.getElementById('AppContainer');
     if (element != null){
-      const scrollTo = (element.scrollHeight / 4) * ref;
+      const scrollTo = (element.scrollHeight / 5) * ref;
       element.scrollTo({
         top: scrollTo,
         behavior: "smooth"
@@ -22,12 +22,13 @@ export const NavBar = (props: Props) => {
       if (element != null) {
         var position = element.scrollTop;
         var total = element.scrollHeight;
-        var elementHeight = total / 4;
+        var elementHeight = total / 5;
 
         if (position <= elementHeight * 0.7) setActive(0);
         else if (position >= elementHeight * 0.7 && position < elementHeight * 1.7) setActive(1);
         else if (position >= elementHeight * 1.7 && position < elementHeight * 2.7) setActive(2);
-        else if (position >= elementHeight * 2.7) setActive(3);
+        else if (position >= elementHeight * 2.7 && position < elementHeight * 3.7) setActive(3);
+        else if (position >= elementHeight * 3.7) setActive(4);
       }
     }, 250);
     return () => clearInterval(interval);
@@ -37,8 +38,9 @@ export const NavBar = (props: Props) => {
     <Topnav>
       <NavElement active={active === 0} onClick={() => setScrollPosition(0)}>Home</NavElement>
       <NavElement active={active === 1} onClick={() => setScrollPosition(1)}>About</NavElement>
-      <NavElement active={active === 2} onClick={() => setScrollPosition(2)}>Education</NavElement>
-      <NavElement active={active === 3} onClick={() => setScrollPosition(3)}>Contact</NavElement>
+      <NavElement active={active === 2} onClick={() => setScrollPosition(2)}>Work</NavElement>
+      <NavElement active={active === 3} onClick={() => setScrollPosition(3)}>Education</NavElement>
+      <NavElement active={active === 4} onClick={() => setScrollPosition(4)}>Contact</NavElement>
     </Topnav>
   );
 };
